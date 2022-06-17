743b4bc1cf01fadea14734b896df4e0cf90f7393
Anthony Sutton <anthony.sutton95@gmail.com>
2022-06-17 07:11:52 -0700

    feat(CRUD): all completed for books

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 - Built with ExpressJS and Prisma
 - [Tutorial Link](https://youtu.be/PM58NEMJgMw?list=PLL1u1k_zVEQngOXcUjWi1VW9KkWxHFBvn)
-- [Latest Progress](https://youtu.be/PM58NEMJgMw?list=PLL1u1k_zVEQngOXcUjWi1VW9KkWxHFBvn&t=4959)
 
 ## Known Bugs
 
--- src/book/book.router.ts
+++ src/book/book.router.ts
@@ -3,6 +3,7 @@ import type { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 
 import * as BookService from './book.service';
+import { request } from 'http';
 
 export const bookRouter = express.Router();
 
@@ -11,6 +12,86 @@ bookRouter.get('/', async (request: Request, response: Response) => {
   try {
     const books = await BookService.listBooks();
     return response.status(200).json(books);
+  } catch (error: any) {
+    return response.status(500).json(error.message);
+  }
+});
+
+// GET: book by id
+bookRouter.get('/:id', async (request: Request, response: Response) => {
+  const id: number = parseInt(request.params.id, 10);
+
+  try {
+    const book = await BookService.getBook(id);
+    if (book) {
+      return response.status(200).json(book);
+    }
+
+    return response.status(404).json({ message: 'Book could not be found' });
+  } catch (error: any) {
+    return response.status(500).json(error.message);
+  }
+});
+
+// POST: create new book
+bookRouter.post(
+  '/',
+  body('title').isString(),
+  body('authorId').isInt(),
+  body('datePublished').isDate().toDate(),
+  body('isFiction').isBoolean(),
+  async (request: Request, response: Response) => {
+    const errors = validationResult(request);
+
+    if (!errors.isEmpty()) {
+      return response.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const book = request.body;
+      const newBook = await BookService.createBook(book);
+      return response.status(201).json(newBook);
+    } catch (error: any) {
+      return response.status(500).json(error.message);
+    }
+  }
+);
+
+// PUT: update book
+bookRouter.put(
+  '/:id',
+  body('title').isString(),
+  body('authorId').isInt(),
+  body('datePublished').isDate().toDate(),
+  body('isFiction').isBoolean(),
+  async (request: Request, response: Response) => {
+    const errors = validationResult(request);
+
+    if (!errors.isEmpty()) {
+      return response.status(400).json({ errors: errors.array() });
+    }
+
+    const id: number = parseInt(request.params.id, 10);
+
+    try {
+      const book = request.body;
+      const updatedBook = await BookService.updateBook(book, id);
+      return response.status(201).json(updatedBook);
+    } catch (error: any) {
+      return response.status(500).json(error.message);
+    }
+  }
+);
+
+// DELETE: delete book
+bookRouter.delete('/:id', async (request: Request, response: Response) => {
+  const id: number = parseInt(request.params.id, 10);
+
+  try {
+    await BookService.deleteBook(id);
+    return response
+      .status(204)
+      .json({ message: 'Book was successfully deleted' });
   } catch (error: any) {
     return response.status(500).json(error.message);
   }
